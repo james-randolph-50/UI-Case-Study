debbdea8d7e41680178f8a2eab906db9ad7b1a73
James <james.randolph2016@gmail.com>
2019-04-12 13:26:34 -0400

    add product highlights componnet

---
+++ user-interface/src/components/ProductHighlights.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductHighlights extends Component {
+
+    constructor(props) {
+        super(props);
+            this.state = {
+                data: null
+            }
+        }
+}
+
+export default ProductHighlights;
