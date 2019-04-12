fe52846927ef0cf83b5ac511ce5c8cfb1e4b54a1
James <james.randolph2016@gmail.com>
2019-04-12 15:47:05 -0400

    Promos.js add

---
+++ user-interface/src/components/Promos.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Promos extends Component {
+
+    constructor(props) {
+        super(props);
+            this.state = {
+                data: null,
+            }
+        }
+
+    
+}
+
+export default Promos;
--- user-interface/src/components/PurchaseOptions.js
+++ user-interface/src/components/PurchaseOptions.js
@@ -19,7 +19,21 @@ class PurchaseOptions extends Component {
         }
 
         render() {
-            
+            const pCCode = this.state.data ? parseInt(this.state.data.purchasingChannelCode, 10) : null;
+        
+            const addToCartEnabled = Number.isInteger(pCCode) ? (pCCode === 0 || pCCode === 1) : null;
+
+            const addToCart = addToCartEnabled ? "Add to Cart is Enabled" : "Add to Cart is Disabled";
+        
+            const pickupInStoreEnabled = Number.isInteger(pCCode) ? (pCCode === 0 || pCCode === 2) : null;
+            const pickupInStore = pickupInStoreEnabled ? "Pickup in Store is Enabled" : "Pickup in Store is Disabled";
+
+            return (
+                <div className="purchase-section">
+                    <div className="purchase-item">{addToCart}</div>
+                    <div className="purchase-item">{pickupInStore}</div>      
+                </div>
+            )
         }
     
 }
