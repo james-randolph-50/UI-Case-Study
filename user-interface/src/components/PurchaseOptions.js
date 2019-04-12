328cc9af267fe2f1b2650291ee78a5b9f0ab73ff
James <james.randolph2016@gmail.com>
2019-04-12 14:19:14 -0400

    purchase options added

---
+++ user-interface/src/components/PurchaseOptions.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class PurchaseOptions extends Component {
+
+    constructor(props) {
+        super(props);
+            this.state = {
+                data: null
+            }
+        }
+    
+}
+
+export default PurchaseOptions;
