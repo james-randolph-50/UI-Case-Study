b2c393e37f7fffd392d8dedf03abcd2172139749
James <james.randolph2016@gmail.com>
2019-04-16 14:25:50 -0400

    list display working

--- user-interface/src/components/ProductHighlights.js
+++ user-interface/src/components/ProductHighlights.js
@@ -1,45 +1,44 @@
 import React, { Component } from 'react';
 import '../App.css';
-// import renderHTML from 'react-render-html';
 
 class ProductHighlights extends Component {
 
-    constructor(props) {
-        super(props);
-            this.state = {
-                data: null
-            }
-        }
-
-    componentWillReceiveProps(newProps) {
-        const index = newProps.selected;
-        const productData = Number.isInteger(index) ? newProps.productData[index] : null;
-            if (productData !== null) {
-                this.setState({ data: productData });
-            }
-        }
-
-    getFeatureList = (itemDescription) => {
-     itemDescription[0].features.map((feature, index) => (
-        <li key={index}>{feature}</li>
-        ))
-      }
-
-    render() {
-        const itemDescription = this.state.data ? this.state.data.ItemDescription : null;
-        const featureList = itemDescription ? this.getFeatureList(itemDescription) : null;
-        console.log(itemDescription);
-        console.log('item description = ' + itemDescription);
-
-        return (
-            <div className="product-highlights-container">
-              <div className="product-highlights-title">product highlights</div>
-              <ul className="product-features">
-                {featureList}
-              </ul>
-            </div>
-          )
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
     }
+  }
+
+  componentWillReceiveProps(newProps) {
+    const index = newProps.selected;
+    const productData = Number.isInteger(index) ? newProps.productData[index] : null;
+
+    if (productData !== null) {
+      this.setState({ data: productData });
+    }
+  }
+
+  getFeatureList = ((itemDescription) => {
+    return itemDescription[0].features.map((feature, index) => (
+      <li key={index}>{ feature }</li>
+    ))
+  })
+
+  render() {
+    const itemDescription = this.state.data ? this.state.data.ItemDescription : null;
+    const featureList = itemDescription ? this.getFeatureList(itemDescription) : null;
+
+    return (
+      <div className="product-highlights-container">
+        <div className="product-highlights-title">product highlights</div>
+        <ul className="product-features">
+          {featureList}
+        </ul>
+      </div>
+    )
+  }
+
 }
 
 export default ProductHighlights;
