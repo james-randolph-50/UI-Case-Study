f633aab2287fb6e87559a1f3b4b63599ab3164a8
James <james.randolph2016@gmail.com>
2019-04-16 09:49:28 -0400

    adding to App.js

--- user-interface/src/App.js
+++ user-interface/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import itemJsonData from './item-data.json';
 import Promos from './components/Promos';
 import ProductHighlights from './components/ProductHighlights';
 import Reviews from './components/Reviews';
@@ -8,23 +9,31 @@ import PurchaseOptions from './components/PurchaseOptions';
 import ProductOffers from './components/ProductOffers';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+      this.state = {
+        allData: null,
+        selectedIndex: null
+      }
+  }
+
+  async componentDidMount() {
+    const catalogData = await itemJsonData ? itemJsonData.CatalogEntryView : null;
+    const index = catalogData ? 0 : null;
+    if (Number.isInteger(index)) {
+      this.setState({ allData: catalogData, selectedIndex: index });
+    }
+  } 
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="product-container">
+        <div className="product-info-item">
+          <SelectedProductContainer productData={this.state.allData}
+          selected={this.state.selectedIndex} />
+        </div>
       </div>
     );
   }
