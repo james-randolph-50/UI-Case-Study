2e050e06323cf3f1fd818024628c3f6c89bd637b
James <james.randolph2016@gmail.com>
2019-04-16 13:59:09 -0400

    featureList is null

--- user-interface/src/components/ProductHighlights.js
+++ user-interface/src/components/ProductHighlights.js
@@ -14,20 +14,22 @@ class ProductHighlights extends Component {
     componentWillReceiveProps(newProps) {
         const index = newProps.selected;
         const productData = Number.isInteger(index) ? newProps.productData[index] : null;
-        if (productData !== null) {
-            this.setState({ data: productData });
+            if (productData !== null) {
+                this.setState({ data: productData });
+            }
         }
-    }
 
-    getFeatureList = ((itemDescription) => {
-        return itemDescription[0].features.map((feature, index) => (
-            <li key={index}>{feature}</li>
+    getFeatureList = (itemDescription) => {
+     itemDescription[0].features.map((feature, index) => (
+        <li key={index}>{feature}</li>
         ))
-    })
+      }
 
     render() {
-        const itemDescription = this.state.data ? this.state.data.itemDescription : null;
+        const itemDescription = this.state.data ? this.state.data.ItemDescription : null;
         const featureList = itemDescription ? this.getFeatureList(itemDescription) : null;
+        console.log(itemDescription);
+        console.log('item description = ' + itemDescription);
 
         return (
             <div className="product-highlights-container">
